f6346699aa899fc3a7064651b379da76dfbfc0f7
Aaron de Boer <aaron.deboer@stu.ocsb.ca>
2024-10-17 10:42:28 -0400

    so many dice

--- main.js
+++ main.js
@@ -9,12 +9,43 @@
 'use strict';
 
 /*** Event Listeners ***/
-
-
+document.getElementById("d4").addEventListener("click", roll_d4);
+document.getElementById("d6").addEventListener("click", roll_d6);
+document.getElementById("d8").addEventListener("click", roll_d8);
+document.getElementById("d10").addEventListener("click", roll_d10);
+document.getElementById("d12").addEventListener("click", roll_d12);
+document.getElementById("d20").addEventListener("click", roll_d20);
+document.getElementById("enter").addEventListener("click",greeting);    
 /*** Functions ***/
-
+function roll_d4() {
+    let roll = randInt(1,4);
+    document.getElementById("die_roll").textContent = roll;
+}
+function roll_d6() {
+    let roll = randInt(1,6);
+    document.getElementById("die_roll").textContent = roll;
+}
+function roll_d8() {
+    let roll = randInt(1,8);
+    document.getElementById("die_roll").textContent = roll;
+}
+function roll_d10() {
+    let roll = randInt(1,10);
+    document.getElementById("die_roll").textContent = roll;
+}
+function roll_d12() {
+    let roll = randInt(1,12);
+    document.getElementById("die_roll").textContent = roll;
+}
+function roll_d20() {
+    let roll = randInt(1,20);
+    document.getElementById("die_roll").textContent = roll;
+}
+function greeting() {
+    let user_name = document.getElementById("user_input").value;
+    document.getElementById("greeting").textContent = `Hi there, ${user_name}! Welcome to your personal hell!`
+}
 // Return a random integer from `min` to `max`, inclusive
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
