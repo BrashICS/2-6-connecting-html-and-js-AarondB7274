2b4731656068dfcaf812597d70c5f91bf1878f19
Aaron de Boer <aaron.deboer@stu.ocsb.ca>
2024-10-18 11:08:34 -0400

    im going to smite myself

--- main.js
+++ main.js
@@ -15,7 +15,9 @@ document.getElementById("d8").addEventListener("click", roll_d8);
 document.getElementById("d10").addEventListener("click", roll_d10);
 document.getElementById("d12").addEventListener("click", roll_d12);
 document.getElementById("d20").addEventListener("click", roll_d20);
-document.getElementById("enter").addEventListener("click",greeting);    
+document.getElementById("d100").addEventListener("click", roll_d100);
+document.getElementById("enter").addEventListener("click",greeting);
+document.getElementById("dynamic_image").addEventListener("click",image_swap);
 /*** Functions ***/
 function roll_d4() {
     let roll = randInt(1,4);
@@ -41,9 +43,18 @@ function roll_d20() {
     let roll = randInt(1,20);
     document.getElementById("die_roll").textContent = roll;
 }
+function roll_d100() {
+    let roll = randInt(1,100);
+    document.getElementById("die_roll").textContent = roll;
+}
 function greeting() {
     let user_name = document.getElementById("user_input").value;
     document.getElementById("greeting").textContent = `Hi there, ${user_name}! Welcome to your personal hell!`
+    document.getElementById("user_input").value = ""
+//  document.getElementById("user_input").focus
+}
+function image_swap() {
+    document.getElementById("dynamic_image").scr = "./images/thisisfine.jpg"
 }
 // Return a random integer from `min` to `max`, inclusive
 function randInt(min, max) {
